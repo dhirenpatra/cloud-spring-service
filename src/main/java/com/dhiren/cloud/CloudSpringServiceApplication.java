55d1223762bb56b3849d7892b1c0a004919c53a4
Dhiren Kumar Patra <51794736+dhirenpatra@users.noreply.github.com>
2024-02-27 23:01:14 +0530

    Initial commit

---
+++ src/main/java/com/dhiren/cloud/CloudSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dhiren.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSpringServiceApplication.class, args);
+	}
+
+}
