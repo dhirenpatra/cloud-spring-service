1d0043afc3cc413894eeaa073b240680d179827d
Dhiren <dhiren.patra@outlook.com>
2024-02-28 20:11:30 +0530

    basic setup for library book event producer

--- src/main/java/com/dhiren/cloud/model/LibraryEvent.java
+++ src/main/java/com/dhiren/cloud/model/LibraryEvent.java
@@ -4,7 +4,7 @@ import com.dhiren.cloud.enums.LibraryEventType;
 import jakarta.validation.constraints.NotNull;
 
 public record LibraryEvent (
-        @NotNull Integer libraryEventId,
+        @NotNull(message = "libraryEventId can't be null or empty. Please provide an id") Integer libraryEventId,
         LibraryEventType libraryEventType,
         Book book) {
 }
