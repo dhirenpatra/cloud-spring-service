9b0559304617249a31d8fc6b024ef71c54959497
Dhiren <dhiren.patra@outlook.com>
2024-03-02 01:06:36 +0530

    basic setup for library book event producer

--- src/main/java/com/dhiren/cloud/repo/FailedRecordRepository.java
+++ src/main/java/com/dhiren/cloud/repo/FailedRecordRepository.java
@@ -1,9 +1,13 @@
 package com.dhiren.cloud.repo;
 
 import com.dhiren.cloud.entity.FailedRecord;
+import com.dhiren.cloud.enums.RecordStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FailedRecordRepository extends JpaRepository<FailedRecord, Integer> {
+    List<FailedRecord> findAllByStatus(RecordStatus recordStatus);
 }
